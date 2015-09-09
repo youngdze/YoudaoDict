605dbd813d471be138003d2eaedc778824b02d3b
youngdze <youngdze94@gmail.com>
2015-09-09 10:42:09 +0800

    class static

--- js/bubble.js
+++ js/bubble.js
@@ -1,163 +1,166 @@
 "use strict";
 
 {
-    let modules = {};
-
-    modules.prototype.getTemplate = function () {
-        return new Promise(function (resolve, reject) {
-            let req = new XMLHttpRequest();
-            let templateUrl = chrome.extension.getURL('template/bubble.html');
-            req.open('GET', templateUrl);
-            req.onload = function () {
-                if (Object.is(req.readyState, 4)) {
-                    if (Object.is(req.status, 200)) {
-                        resolve(req.response);
-                    } else {
-                        reject(req.req.status);
-                    }
-                }
-            };
-            req.onerror = function () {
-                reject('Template not found');
-            };
-            req.send();
-        });
-    };
-
-    modules.prototype.injectData = function (template, data) {
-        Mustache.parse(template);
-        return Mustache.render(template, data);
-    };
-
-    modules.prototype.renderBubble = function (rendered) {
-        if (document.getElementById('y-bubble')) {
-            let unnecessaryBubble = document.getElementById('y-bubble');
-            unnecessaryBubble.parentNode.removeChild(unnecessaryBubble);
-        }
-
-        let selection = window.getSelection();
-        let range = selection.getRangeAt(0);
-        let rect = range.getBoundingClientRect();
-        let bubbleLeft, bubbleTop, arrowRelativeLeft;
-
-        let dummy = document.createElement('DIV');
-        dummy.innerHTML = rendered.trim();
-        let dummyChild = dummy.childNodes;
-        document.body.appendChild(dummyChild[0]);
-
-        let bubble = document.getElementById('y-bubble');
-
-        bubbleLeft = rect.left + rect.width / 2 - bubble.offsetWidth / 2;
-
-        if (bubbleLeft < 5) {
-            bubbleLeft = 5;
-            arrowRelativeLeft = rect.left + rect.width / 2 - 15;
-        } else if ((bubbleLeft + bubble.offsetWidth) > (document.body.offsetWidth + 5)) {
-            bubbleLeft = document.body.offsetWidth - bubble.offsetWidth - 5;
-            arrowRelativeLeft = rect.left - bubbleLeft - 10 + rect.width / 2;
-        } else {
-            arrowRelativeLeft = bubble.offsetWidth / 2 - 10;
-        }
-
-        if (rect.top < bubble.offsetHeight) {
-            bubbleTop = rect.top + window.scrollY + rect.height + 8;
-
-            let bubbleArrow = document.getElementById('y-arrow');
-            bubbleArrow.style.borderBottom = '10px solid rgba(13, 13, 13, .8)';
-            bubbleArrow.style.borderTop = 0;
-            bubbleArrow.style.top = '-8px';
-            bubbleArrow.style.left = arrowRelativeLeft + 'px';
-        } else {
-            bubbleTop = rect.top + window.scrollY - bubble.offsetHeight - 10;
-
-            let bubbleArrow = document.getElementById('y-arrow');
-            bubbleArrow.style.borderBottom = 0;
-            bubbleArrow.style.borderTop = '10px solid rgba(13, 13, 13, .8)';
-            bubbleArrow.style.top = bubble.offsetHeight + 'px';
-            bubbleArrow.style.left = arrowRelativeLeft + 'px';
-        }
-
-        bubble.style.left = bubbleLeft + 'px';
-        bubble.style.top = bubbleTop + 'px';
-
-
-        document.addEventListener('click', function (ev) {
-            if (bubble.parentNode) {
-                bubble.parentNode.removeChild(bubble);
-            }
-        });
-        bubble.addEventListener('click', function (ev) {
-            ev.stopPropagation();
-        });
-    };
-
-    modules.prototype.enableDblclick = function () {
-        document.addEventListener('dblclick', function (ev) {
-            //let [from, resType, query, youdaoKey] = ['YoungdzeBlog', 'json', window.getSelection().toString().trim(), 498418215];
-            let from = 'YoungdzeBlog', resType = 'json', query = window.getSelection().toString().trim(), youdaoKey = 498418215;
-            if (_.isEmpty(query)) return;
-
-            modules.getTemplate()
-                .then(function (template) {
-                    let rendered;
-                    rendered = modules.injectData(template, {loading: true});
-                    modules.renderBubble(rendered);
-
-                    let youdao = new Youdao(from, youdaoKey, resType, query);
-                    youdao.getContent()
-                        .then(function (data) {
-                            data.loading = false;
-                            rendered = modules.injectData(template, data);
-                            modules.renderBubble(rendered);
-                        }).catch(function (err) {
-                            rendered = modules.injectData(template, {explains: err});
-                            modules.renderBubble(rendered);
-                        });
-                });
-        });
-    };
-
-    modules.prototype.enableKeydown = function () {
-        let map = [];
-        document.addEventListener('keydown', function (ev) {
-            map.push(ev.keyCode);
-        });
-        document.addEventListener('keyup', function (ev) {
-            if (Object.is(_.size(map), 1) && Object.is(map[0], 17)) {
-                //let [from, resType, query, youdaoKey] = ['YoungdzeBlog', 'json', window.getSelection().toString().trim(), 498418215];
-                let from = 'YoungdzeBlog', resType = 'json', query = window.getSelection().toString().trim(), youdaoKey = 498418215;
-                if (_.isEmpty(query)) return;
-
-                modules.getTemplate()
-                    .then(function (template) {
-                        let rendered;
-                        rendered = modules.injectData(template, {loading: true});
-                        modules.renderBubble(rendered);
-
-                        let youdao = new Youdao(from, youdaoKey, resType, query);
-                        youdao.getContent()
-                            .then(function (data) {
-                                data.loading = false;
-                                rendered = modules.injectData(template, data);
-                                modules.renderBubble(rendered);
-                            }).catch(function (err) {
-                                rendered = modules.injectData(template, {explains: err});
-                                modules.renderBubble(rendered);
-                            });
-                    });
-            }
-            map = [];
-        });
-    };
-
-    {
-        chrome.storage.sync.get(function (items) {
-            if (items.dblclick) modules.enableDblclick();
-            if (items.ctrl) modules.enableKeydown();
-        })
+  class Module {
+    constructor(){
     }
+
+    static getTemplate(){
+      return new Promise(function( resolve, reject ){
+        let req = new XMLHttpRequest();
+        let templateUrl = chrome.extension.getURL('template/bubble.html');
+        req.open('GET', templateUrl);
+        req.onload = function(){
+          if(Object.is(req.readyState, 4)){
+            if(Object.is(req.status, 200)){
+              resolve(req.response);
+            } else{
+              reject(req.req.status);
+            }
+          }
+        };
+        req.onerror = function(){
+          reject('Template not found');
+        };
+        req.send();
+      });
+    }
+
+    static injectData( template, data ){
+      Mustache.parse(template);
+      return Mustache.render(template, data);
+    }
+
+    static renderBubble( rendered ){
+      if(document.getElementById('y-bubble')){
+        let unnecessaryBubble = document.getElementById('y-bubble');
+        unnecessaryBubble.parentNode.removeChild(unnecessaryBubble);
+      }
+
+      let selection = window.getSelection();
+      let range = selection.getRangeAt(0);
+      let rect = range.getBoundingClientRect();
+      let bubbleLeft, bubbleTop, arrowRelativeLeft;
+
+      let dummy = document.createElement('DIV');
+      dummy.innerHTML = rendered.trim();
+      let dummyChild = dummy.childNodes;
+      document.body.appendChild(dummyChild[0]);
+
+      let bubble = document.getElementById('y-bubble');
+
+      bubbleLeft = rect.left + rect.width / 2 - bubble.offsetWidth / 2;
+
+      if(bubbleLeft < 5){
+        bubbleLeft = 5;
+        arrowRelativeLeft = rect.left + rect.width / 2 - 15;
+      } else if((bubbleLeft + bubble.offsetWidth) > (document.body.offsetWidth + 5)){
+        bubbleLeft = document.body.offsetWidth - bubble.offsetWidth - 5;
+        arrowRelativeLeft = rect.left - bubbleLeft - 10 + rect.width / 2;
+      } else{
+        arrowRelativeLeft = bubble.offsetWidth / 2 - 10;
+      }
+
+      if(rect.top < bubble.offsetHeight){
+        bubbleTop = rect.top + window.scrollY + rect.height + 8;
+
+        let bubbleArrow = document.getElementById('y-arrow');
+        bubbleArrow.style.borderBottom = '10px solid rgba(13, 13, 13, .8)';
+        bubbleArrow.style.borderTop = 0;
+        bubbleArrow.style.top = '-8px';
+        bubbleArrow.style.left = arrowRelativeLeft + 'px';
+      } else{
+        bubbleTop = rect.top + window.scrollY - bubble.offsetHeight - 10;
+
+        let bubbleArrow = document.getElementById('y-arrow');
+        bubbleArrow.style.borderBottom = 0;
+        bubbleArrow.style.borderTop = '10px solid rgba(13, 13, 13, .8)';
+        bubbleArrow.style.top = bubble.offsetHeight + 'px';
+        bubbleArrow.style.left = arrowRelativeLeft + 'px';
+      }
+
+      bubble.style.left = bubbleLeft + 'px';
+      bubble.style.top = bubbleTop + 'px';
+
+
+      document.addEventListener('click', function( ev ){
+        if(bubble.parentNode){
+          bubble.parentNode.removeChild(bubble);
+        }
+      });
+      bubble.addEventListener('click', function( ev ){
+        ev.stopPropagation();
+      });
+    }
+
+    static enableDblclick(){
+      document.addEventListener('dblclick', function( ev ){
+        //let [from, resType, query, youdaoKey] = ['YoungdzeBlog', 'json', window.getSelection().toString().trim(), 498418215];
+        let from = 'YoungdzeBlog', resType = 'json', query = window.getSelection().toString().trim(), youdaoKey = 498418215;
+        if(_.isEmpty(query)) return;
+
+        Module.getTemplate()
+          .then(function( template ){
+            let rendered;
+            rendered = Module.injectData(template, {loading:true});
+            Module.renderBubble(rendered);
+
+            let youdao = new Youdao(from, youdaoKey, resType, query);
+            youdao.getContent()
+              .then(function( data ){
+                data.loading = false;
+                rendered = Module.injectData(template, data);
+                Module.renderBubble(rendered);
+              }).catch(function( err ){
+                rendered = Module.injectData(template, {explains:err});
+                Module.renderBubble(rendered);
+              });
+          });
+      });
+    }
+
+    static enableKeydown(){
+      let map = [];
+      document.addEventListener('keydown', function( ev ){
+        map.push(ev.keyCode);
+      });
+      document.addEventListener('keyup', function( ev ){
+        if(Object.is(_.size(map), 1) && Object.is(map[0], 17)){
+          //let [from, resType, query, youdaoKey] = ['YoungdzeBlog', 'json', window.getSelection().toString().trim(), 498418215];
+          let from = 'YoungdzeBlog', resType = 'json', query = window.getSelection().toString().trim(), youdaoKey = 498418215;
+          if(_.isEmpty(query)) return;
+
+          Module.getTemplate()
+            .then(function( template ){
+              let rendered;
+              rendered = Module.injectData(template, {loading:true});
+              Module.renderBubble(rendered);
+
+              let youdao = new Youdao(from, youdaoKey, resType, query);
+              youdao.getContent()
+                .then(function( data ){
+                  data.loading = false;
+                  rendered = Module.injectData(template, data);
+                  Module.renderBubble(rendered);
+                }).catch(function( err ){
+                  rendered = Module.injectData(template, {explains:err});
+                  Module.renderBubble(rendered);
+                });
+            });
+        }
+        map = [];
+      });
+    }
+  }
+
+  {
+    chrome.storage.sync.get(function( items ){
+      if(items.dblclick) Module.enableDblclick();
+      if(items.ctrl) Module.enableKeydown();
+    })
+  }
 }
 
-function y_playSound(wav_file) {
-    document.getElementById('y-bubble-wav-wrapper').innerHTML = '<embed src="' + wav_file + '" type="audio/wav" autostart="true" style="width: 0; height: 0;">';
+function y_playSound( wav_file ){
+  document.getElementById('y-bubble-wav-wrapper').innerHTML = '<embed src="' + wav_file + '" type="audio/wav" autostart="true" style="width: 0; height: 0;">';
 }
--- js/popup.js
+++ js/popup.js
@@ -1,110 +1,113 @@
 "use strict";
 
-define(['underscore'], function (_) {
-    let modules = {};
+define(['underscore'], function( _ ){
+  class Module {
+    constructor(){
+    }
 
-    modules.prototype.translate = function (query) {
-        return new Promise(function (resolve, reject) {
-            let url = 'https://fanyi.youdao.com/openapi.do?keyfrom=YoungdzeBlog&key=498418215&type=data&doctype=json&version=1.1&q=';
-            let req = new XMLHttpRequest();
-            req.open('GET', url + encodeURIComponent(query));
-            req.responseType = 'json';
-            req.onload = function () {
-                if (Object.is(req.readyState, 4)) {
-                    if (Object.is(req.status, 200)) {
-                        resolve(req.response);
-                    } else {
-                        reject('Network error: ' + req.status);
-                    }
-                }
-            };
-            req.onerror = function () {
-                reject('Network error.');
-            };
-            req.send();
-        });
-    };
-
-    modules.prototype.isLoad = function (isLoading) {
-        document.getElementById('loading').hidden = !isLoading;
-        document.getElementById('resultBox').hidden = isLoading;
-    };
-
-    modules.prototype.renderResult = function (resultJson) {
-        modules.isLoad(false);
-        let basic = document.getElementById('basic');
-        while (!_.isNull(basic.nextElementSibling)) basic.nextElementSibling.remove();
-
-        if (!resultJson) {
-            basic.textContent = 'Nothing found.';
-            return;
-        }
-        if (_.isString(resultJson)) {
-            basic.textContent = resultJson.toString();
-            return;
-        }
-        if (!resultJson.basic) {
-            basic.textContent = resultJson.translation[0];
-            return;
-        }
-        if (!_.isUndefined(resultJson.basic.phonetic)) {
-            basic.innerHTML = '<code class="pronoun">/' + (resultJson.basic.phonetic.split(';'))[0] + '/</code>';
-        } else {
-            basic.innerHTML = '';
-        }
-
-        for (let i = 0; i < resultJson.basic.explains.length; i++) {
-            if (i) {
-                basic.innerHTML += '<br>' + resultJson.basic.explains[i];
-            } else {
-                basic.innerHTML += resultJson.basic.explains[i];
+    static translate( query ){
+      return new Promise(function( resolve, reject ){
+        let url = 'https://fanyi.youdao.com/openapi.do?keyfrom=YoungdzeBlog&key=498418215&type=data&doctype=json&version=1.1&q=';
+        let req = new XMLHttpRequest();
+        req.open('GET', url + encodeURIComponent(query));
+        req.responseType = 'json';
+        req.onload = function(){
+          if(Object.is(req.readyState, 4)){
+            if(Object.is(req.status, 200)){
+              resolve(req.response);
+            } else{
+              reject('Network error: ' + req.status);
             }
+          }
+        };
+        req.onerror = function(){
+          reject('Network error.');
+        };
+        req.send();
+      });
+    }
+
+    static isLoad( isLoading ){
+      document.getElementById('loading').hidden = !isLoading;
+      document.getElementById('resultBox').hidden = isLoading;
+    }
+
+    static renderResult( resultJson ){
+      Module.isLoad(false);
+      let basic = document.getElementById('basic');
+      while(!_.isNull(basic.nextElementSibling)) basic.nextElementSibling.remove();
+
+      if(!resultJson){
+        basic.textContent = 'Nothing found.';
+        return;
+      }
+      if(_.isString(resultJson)){
+        basic.textContent = resultJson.toString();
+        return;
+      }
+      if(!resultJson.basic){
+        basic.textContent = resultJson.translation[0];
+        return;
+      }
+      if(!_.isUndefined(resultJson.basic.phonetic)){
+        basic.innerHTML = '<code class="pronoun">/' + (resultJson.basic.phonetic.split(';'))[0] + '/</code>';
+      } else{
+        basic.innerHTML = '';
+      }
+
+      for(let i = 0; i < resultJson.basic.explains.length; i++){
+        if(i){
+          basic.innerHTML += '<br>' + resultJson.basic.explains[i];
+        } else{
+          basic.innerHTML += resultJson.basic.explains[i];
+        }
+      }
+
+      let webTranslation = resultJson.web;
+      for(let i = 0; i < _.size(webTranslation); i++){
+        var pEle = document.createElement('P');
+        pEle.className = 'web-translation';
+        pEle.textContent = webTranslation[i].key + ':  ' + webTranslation[i].value.join(', ');
+        document.getElementById('resultField').appendChild(pEle);
+      }
+    }
+
+    static processResult( queryInput ){
+      let query = queryInput.value;
+      if(!query) return;
+
+      Module.isLoad(true);
+      Module.translate(query).then(function( res ){
+        Module.renderResult(res);
+      }).catch(function( err ){
+        Module.renderResult(err);
+      });
+    }
+
+    static onLoad(){
+      let form = document.forms.namedItem('dictForm');
+      let queryInput = form.query;
+      let timeout;
+      queryInput.focus();
+
+      form.addEventListener('submit', function( ev ){
+        ev.preventDefault();
+        modules.processResult(queryInput);
+      });
+
+      queryInput.addEventListener('keyup', function( ev ){
+        if(Object.is(ev.keyCode, 13)) return;
+        if(timeout){
+          clearTimeout(timeout);
+          timeout = null;
         }
 
-        let webTranslation = resultJson.web;
-        for (let i = 0; i < _.size(webTranslation); i++) {
-            var pEle = document.createElement('P');
-            pEle.className = 'web-translation';
-            pEle.textContent = webTranslation[i].key + ':  ' + webTranslation[i].value.join(', ');
-            document.getElementById('resultField').appendChild(pEle);
-        }
-    };
+        timeout = setTimeout(function(){
+          modules.processResult(queryInput);
+        }, 700);
+      });
+    }
+  }
 
-    modules.prototype.processResult = function (queryInput) {
-        let query = queryInput.value;
-        if (!query) return;
-
-        modules.isLoad(true);
-        modules.translate(query).then(function (res) {
-            modules.renderResult(res);
-        }).catch(function (err) {
-            modules.renderResult(err);
-        });
-    };
-
-    modules.prototype.onLoad = function () {
-        let form = document.forms.namedItem('dictForm');
-        let queryInput = form.query;
-        let timeout;
-        queryInput.focus();
-
-        form.addEventListener('submit', function (ev) {
-            ev.preventDefault();
-            modules.processResult(queryInput);
-        });
-
-        queryInput.addEventListener('keyup', function (ev) {
-            if (Object.is(ev.keyCode, 13)) return;
-            if (timeout) {
-                clearTimeout(timeout);
-                timeout = null;
-            }
-
-            timeout = setTimeout(function () {
-                modules.processResult(queryInput);
-            }, 700);
-        });
-    };
-
-    return modules;
+  return Module;
 });
