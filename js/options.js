584c85dc8ef5e5ebd4be0fefa6425c80bd59c970
youngdze <youngdze94@gmail.com>
2015-11-01 23:39:32 +0800

    add loading status

--- js/options.js
+++ js/options.js
@@ -1,36 +1,34 @@
 "use strict";
 
+class Module {
+    constructor() {
+    }
+
+    static save_options() {
+        let dblclick = document.querySelector('#dblclick').checked;
+        let ctrl = document.querySelector('#ctrl').checked;
+        chrome.storage.sync.set({
+            dblclick: dblclick,
+            ctrl: ctrl
+        }, function () {
+            Materialize.toast('设置成功', 500);
+        });
+    }
+
+    static restore_options() {
+        chrome.storage.sync.get(function (items) {
+            document.querySelector('#dblclick').checked = items.dblclick;
+            document.querySelector('#ctrl').checked = items.ctrl;
+        });
+    }
+}
+
 {
-  class Module {
-    constructor(){
-    }
-
-    static save_options(){
-      let dblclick = document.querySelector('#dblclick').checked;
-      let ctrl = document.querySelector('#ctrl').checked;
-      chrome.storage.sync.set({
-        dblclick:dblclick,
-        ctrl    :ctrl
-      }, function(){
-        Materialize.toast('设置成功', 500);
-      });
-    }
-
-    static restore_options(){
-      chrome.storage.sync.get(function( items ){
-        document.querySelector('#dblclick').checked = items.dblclick;
-        document.querySelector('#ctrl').checked = items.ctrl;
-      });
-    }
-  }
-
-  {
     document.addEventListener('DOMContentLoaded', Module.restore_options);
 
     let options = document.querySelectorAll('input[type=checkbox]');
-    Object.keys(options).map(function( key ){
-      // detect if is number
-      if(!isNaN(key - 0)) options[key].addEventListener('click', Module.save_options);
+    Object.keys(options).forEach(function (key) {
+        // detect if is number
+        if (!isNaN(key - 0)) options[key].addEventListener('click', Module.save_options);
     });
-  }
 }
