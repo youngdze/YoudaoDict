58a16051cb4a1fadd703df974b8b1d7091c1993d
youngdze <youngdze94@gmail.com>
2016-01-02 21:57:02 +0800

    gulp using es6 syntax

---
+++ gulpfile.babel.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import gulp from 'gulp';
+import jade from 'gulp-jade';
+import imagemin from 'gulp-imagemin';
+import pngquant from 'imagemin-pngquant';
+import webpack from 'webpack-stream';
+import webpackConf from './webpack.config.js';
+
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+
+gulp.task('manifest', () => {
+  gulp.src('manifest.json').pipe(gulp.dest('build'));
+});
+
+gulp.task('moveLib', () => {
+  gulp.src('src/lib/**/*.js').pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('jade', () => {
+  gulp.src(['src/tpl/popup.jade', 'src/tpl/options.jade'])
+    .pipe(jade())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('imagemin', () => {
+  gulp.src('src/img/icon.png')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('webpack', () => {
+  gulp.src('./src/script/*.js')
+    .pipe(webpack(webpackConf))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['manifest', 'moveLib', 'jade', 'imagemin', 'webpack'], () => {
+  if(Object.is(NODE_ENV, 'dev')) {
+    gulp.watch(['./manifest.json'], ['manifest']);
+    gulp.watch(['./src/tpl/*.jade'], ['jade']);
+    gulp.watch(['./src/tpl/*.jade', './src/style/*.scss', './src/script/**/*.js'], ['webpack']);
+  }
+});
