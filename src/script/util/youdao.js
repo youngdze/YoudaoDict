9835c1d87199af9cae5dba5b431c5f564f4904fc
youngdze <youngdze94@gmail.com>
2016-01-10 21:12:45 +0800

    mocha test

--- src/script/util/youdao.js
+++ src/script/util/youdao.js
@@ -12,8 +12,8 @@ class Youdao {
   }
 
   parseJsonContent(res) {
-    let word, explains, pronoun, wav, relate = [],
-      more;
+    let word, explains, pronoun, wav, relate = [], more;
+    res = (typeof res === 'string') ? JSON.parse(res) : res;
 
     word = res.query;
     if (!res) {
@@ -34,33 +34,33 @@ class Youdao {
   }
 
   parseXmlContent(res) {
-    let word, explains, pronoun, wav, relate = [],
-      more;
+    let word, explains, pronoun, wav, relate = [], more;
+    res = (new DOMParser()).parseFromString(res, 'text/xml');
 
-    word = res.querySelector('query').textContent;
+    word = res.querySelector('query').textContent.toString().trim();
     if (!res) {
       explains = 'Nothing found.';
     } else if (Object.is(typeof res, 'string')) {
-      explains = res.toString();
+      explains = res.toString().trim();
     } else if (!res.querySelectorAll('basic').length) {
-      explains = res.querySelector('translation').querySelector('paragraph').textContent;
+      explains = res.querySelector('translation').querySelector('paragraph').textContent.toString().trim();
     } else {
       let explainsNode = res.querySelector('basic').querySelector('explains').querySelectorAll('ex');
-      explains = explainsNode.map(v => v.textContent);
-      pronoun = res.querySelector('basic').querySelector('phonetic').textContent || undefined;
+      explains = [...explainsNode].map(v => v.textContent.toString().trim());
+      pronoun = res.querySelector('basic').querySelector('phonetic').textContent.toString().trim() || undefined;
       !this.isChinese(word) && (wav = `https://dict.youdao.com/dictvoice?audio=${word}&type=2`);
 
-      let relates = res.querySelector('web').querySelector('explain');
+      let relates = [...res.querySelector('web').querySelector('explain')];
       if (relates.length) {
         relate = relates.map(v => {
           let dummy = {};
-          dummy.key = v.querySelector('key').textContent;
-          dummy.relate = [...v.querySelector('value')].map(v => val.textContent);
+          dummy.key = v.querySelector('key').textContent.toString().trim();
+          dummy.relate = [...v.querySelector('value')].map(v => val.textContent.toString().trim());
           return dummy;
         });
       }
     }
-    more = res.querySelector('query')[0].textContent;
+    more = res.querySelector('query')[0].textContent.toString().trim();
 
     return {word, wav, explains, pronoun, relate, more};
   }
@@ -73,8 +73,13 @@ class Youdao {
         .then(res => {
           if (res.ok) {
             // TODO judge res type
-            res.json().then(data => {
-              let result = _this.parseJsonContent(data);
+            res.text().then(data => {
+              let result;
+              if(Object.is(_this.resType, 'json')) {
+                result = _this.parseJsonContent(data);
+              } else {
+                result = _this.parseXmlContent(data);
+              }
               resolve(result);
             });
           } else {
---
+++ test/youdao.js
@@ -0,0 +1,77 @@
+import {readFileSync} from 'fs';
+import {join as pathJoin} from 'path';
+import 'mocha';
+import {expect} from 'chai';
+import Youdao from '../src/script/util/youdao.js';
+
+describe(`Youdao`, () => {
+  const [from, resType, query, youdaoKey] = [`YoungdzeBlog`, `json`, `test`, 498418215];
+  let youdao = new Youdao(from, youdaoKey, resType, query);
+
+  describe(`#isChinese`, () => {
+    it(`should be Chinese`, () => {
+      expect(youdao.isChinese(`摩卡`)).to.equal(true);
+      expect(youdao.isChinese(`摩卡摩卡摩卡摩卡摩卡摩卡摩卡摩卡摩卡摩卡`)).to.equal(true);
+    });
+
+    it(`should not be Chinese`, () => {
+      expect(youdao.isChinese()).to.equal(false);
+      expect(youdao.isChinese(`mocha`)).to.equal(false);
+      expect(youdao.isChinese(` mocha`)).to.equal(false);
+      expect(youdao.isChinese(` mocha `)).to.equal(false);
+      expect(youdao.isChinese(`摩卡m`)).to.equal(false);
+      expect(youdao.isChinese(`m摩卡`)).to.equal(false);
+      expect(youdao.isChinese(`摩卡 `)).to.equal(false);
+      expect(youdao.isChinese(` 摩卡`)).to.equal(false);
+    });
+  });
+
+  describe(`#parseJsonContent`, () => {
+    let dataJson = JSON.parse(readFileSync(pathJoin(__dirname, `./data.json`), `utf-8`));
+    let parsedJson = youdao.parseJsonContent(dataJson);
+    const urlReg = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/i;
+
+    it(`should has necessary own property`, () => {
+      expect(parsedJson).to.have.ownProperty(`word`);
+      expect(parsedJson).to.have.ownProperty(`explains`);
+      expect(parsedJson).to.have.ownProperty(`pronoun`);
+      expect(parsedJson).to.have.ownProperty(`relate`);
+      expect(parsedJson).to.have.ownProperty(`more`);
+    });
+
+    it(`property "word" is valid`, () => {
+      let word = parsedJson.word;
+      expect(word).to.be.a('string');
+    });
+
+    it('property "wav" is valid', () => {
+      let wav = parsedJson.wav;
+      expect(wav).to.match(urlReg);
+    });
+
+    it('property "explains" valid', () => {
+      let explains = parsedJson.explains;
+      expect(explains).to.be.an('array');
+    });
+
+    it('property "pronoun" is valid', () => {
+      let pronoun = parsedJson.pronoun;
+      expect(pronoun).to.be.a('string');
+    });
+
+    it('property "relate" is valid', () => {
+      let relates = parsedJson.relate;
+      expect(relates).to.be.an('array');
+      relates.forEach(it => {
+        expect(it).to.has.ownProperty('value').and.has.ownProperty('key');
+        expect(it.value).to.be.an('array');
+        expect(it.key).to.be.a('string');
+      });
+    });
+
+    it('property "more" is valid', () => {
+      let more = parsedJson.more;
+      expect(more).to.match(urlReg);
+    });
+  });
+});
